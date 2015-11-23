7cfcbcb6924625fa1724169378f186bc46df81d6
Farhad Ghayour <me@farhadg.com>
2015-11-23 02:12:42 -0500

    add: card, author, background and thumb are setable

--- index.js
+++ index.js
@@ -42,20 +42,49 @@ var RepoCardLib = RepoCardLib || (function() {
 			}
 		}
 
+		/**
+		 * Helper function for setting the content of the given dom element
+		 *
+		 * @param selector
+		 * @param data
+		 * @returns {*}
+		 * @private
+		 */
+
+		function _setContent(selector, data) {
+			return document.getElementsByClassName(selector)[0].innerText = data;
+		}
+
+		function _setStyle(selector, property, value) {
+			var el = document.getElementsByClassName(selector)[0];
+			el.style[property] = value;
+			if (property === 'background') {
+				el.style.backgroundSize = 'cover';
+			}
+			return;
+		}
+
+		/**
+		 * Configures the data based on the params provided to the lib script
+		 */
 
 		RepoCard.prototype.configure = function configure() {
-			var actions = {
+			var watchers = {
 				name: function(name) {
-					document.getElementsByClassName('repo-card__title')[0].innerText = name;
+					return _setContent('repo-card__title', name);
 				},
 				author: function(author) {
-					document.getElementsByClassName('repo-card__author')[0].innerText = author;
+					return _setContent('repo-card__author', author);
+				},
+				info: function(info) {
+					return _setContent('repo-card__content', info);
+				},
+				thumb: function(value) {
+					return _setStyle('repo-card__thumb', 'background', value);
 				}
 			};
 			for (var param in this.params) {
-				if (actions[param]) {
-					actions[param](this.params[param]);
-				}
+				(watchers[param]) && (watchers[param](this.params[param]));
 			}
 		};
 
