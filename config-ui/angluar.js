a8c5c2ff02b32776dcbd6cafadb45681510af2db
Farhad Ghayour <me@farhadg.com>
2015-11-24 00:58:24 -0500

    add: base case for config-ui

--- config-ui/angluar.js
+++ config-ui/angluar.js
@@ -3,6 +3,26 @@ angular.module('MyApp')
 
 .controller('AppCtrl', function($scope) {
 
+  $scope.repoCardData = {
+    repo: null,
+    username: null,
+    title: null,
+    subtitle: null,
+    thumb: null,
+    background: null,
+    info: null,
+    position: {
+      bottom: 20,
+      left: 20
+    }
+  };
+
+  $scope.configure = function() {
+    RepoCard.configure($scope.repoCardData);
+    console.log('configured');
+  };
+
+
   $scope.color = {
     red: Math.floor(Math.random() * 255),
     green: Math.floor(Math.random() * 255),
